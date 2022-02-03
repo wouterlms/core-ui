bd6482d1e4958f6daf6a53b56bc8d5c0ddaadefe
Wouter <wouter.laermans@appwise.be>
2022-02-03 16:16:14 +0100

    FormLabel: added `error` prop

--- src/components/form-label/FormLabel.vue
+++ src/components/form-label/FormLabel.vue
@@ -1,15 +1,24 @@
 <script setup lang="ts">
 interface Props {
   label: string
+  error?: string
 }
 
-withDefaults(defineProps<Props>(), {})
+withDefaults(defineProps<Props>(), {
+  error: undefined,
+})
 </script>
 
 <template>
   <label class="flex flex-col">
-    <span class="mb-0.5 text-secondary text-xs">
+    <span
+      :class="[
+        !!error ? 'text-error' : 'text-secondary'
+      ]"
+      class="mb-0.5 text-xs"
+    >
       {{ label }}
+      <template v-if="!!error">| {{ error }}</template>
     </span>
     <slot />
   </label>
