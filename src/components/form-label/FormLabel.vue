badfbc307d7f773bf7fb610d7b0b72382077ee69
Wouter <wouter.laermans@appwise.be>
2022-02-06 17:02:50 +0100

    FormLabel: Added `position` prop

--- src/components/form-label/FormLabel.vue
+++ src/components/form-label/FormLabel.vue
@@ -2,24 +2,33 @@
 interface Props {
   label: string
   error?: string | boolean | null | undefined
+  position?: 'top' | 'bottom'
 }
 
 withDefaults(defineProps<Props>(), {
   error: undefined,
+  position: 'top',
 })
 </script>
 
 <template>
-  <label class="flex flex-col">
+  <label
+    :class="[
+      position === 'bottom' ? 'flex-col-reverse' : 'flex-col'
+    ]"
+    class="flex"
+  >
     <span
       :class="[
-        !!error ? 'text-error' : 'text-secondary'
+        !!error ? 'text-error' : 'text-secondary',
+        position === 'bottom' ? 'mt-0.5' : 'mb-0.5'
       ]"
-      class="mb-0.5 text-xs"
+      class="text-xs"
     >
       {{ label }}
       <template v-if="typeof error === 'string'">| {{ error }}</template>
     </span>
+
     <slot />
   </label>
 </template>
